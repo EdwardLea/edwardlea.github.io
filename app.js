b2ab79eb179033a9cf8d13c2028d1164e4db005f
edwardlea <leaedward87@gmail.com>
2019-03-19 10:46:47 +0000

    animation added to skills section

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 $(() => {
   AOS.init()
 
-
   console.log('Js loaded')
-  const $codeButton = $('.coding').find('.title')
-  const $codeSection = $('.coding').find('.text')
-  const $outsideButton = $('.outside').find('.title')
-  const $outsideSection = $('.outside').find('.text')
+  const $codeSection = $('.coding')
+  const $outsideSection = $('.outside')
+  const $skillsSection = $('.icon')
+  const $skillsTitle = $('.title')
 
-  function codingClick(e){
-    console.log(e.currentTarget)
-    $codeSection.slideToggle()
-
-
+  function showClick(e){
+    $(e.currentTarget).find('.text').slideToggle()
   }
 
-  $codeButton.on('click', codingClick)
-  $outsideButton.on('click', codingClick)
+  function iconHoverIn(e){
+    $(e.currentTarget).find('i').hide()
+    $(e.currentTarget).find('span').show()
+  }
+  function iconHoverOut(e){
+    $(e.currentTarget).find('i').show()
+    $(e.currentTarget).find('span').hide()
+  }
+
+  $codeSection.on('click', showClick)
+  $outsideSection.on('click', showClick)
+  $skillsSection.on('mouseenter', iconHoverIn)
+  $skillsSection.on('mouseleave', iconHoverOut)
+  $skillsTitle.on('mouseenter', iconHoverIn)
+  $skillsTitle.on('mouseleave', iconHoverOut)
 })
