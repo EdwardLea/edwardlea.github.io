99cbe12c6e846a99b12f3a116ceae1750fee9993
edwardlea <leaedward87@gmail.com>
2019-03-20 09:00:52 +0000

    hero heading animation added

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ $(() => {
   const $outsideSection = $('.outside')
   const $skillsSection = $('.icon')
   const $skillsTitle = $('.title')
+  const $heroHeading = $('.hero').find('h1')
+  const $heroSubHeading = $('.hero').find('h2')
+
+  function splitHeading(element) {
+    const arrayHeading = element.text().split('')
+    const span = arrayHeading.map(letter => {
+      return `<span class = "hero">${letter}</span>`
+    }).join('')
+    element.html(span)
+    // spans = Array.from(document.querySelectorAll('span'))
+    // console.dir(spans)
+    // spans.forEach(function(span, index) {
+    //   setTimeout(function(){
+    //     spans[index].classList.add('hero-active')
+    //     spans[index].classList.remove('hero')
+    //   }, 1000 * (index + 1))
+    // })
+  }
+
+  splitHeading($heroHeading)
+  splitHeading($heroSubHeading)
+
 
   function showClick(e){
     $(e.currentTarget).find('.text').slideToggle()
